adeedb01a0ae52ce3bb8345aee5a02c721722955
Karolina Wasyluk <261637@student.pwr.edu.pl>
2023-04-24 20:57:26 +0200

    poprawki do okien logowania i rejestracji

--- app/src/main/java/com/example/mobilepharmacy/Login.kt
+++ app/src/main/java/com/example/mobilepharmacy/Login.kt
@@ -1,11 +1,32 @@
 package com.example.mobilepharmacy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.view.Menu
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.mobilepharmacy.databinding.ActivityLogin2Binding
+import com.example.mobilepharmacy.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+
+        val leftIcon: ImageView = findViewById(R.id.backButton)
+
+        leftIcon.setOnClickListener{
+            val intent = Intent(this@Login, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
+
+
 }
--- app/src/main/java/com/example/mobilepharmacy/RegisterActivity.kt
+++ app/src/main/java/com/example/mobilepharmacy/RegisterActivity.kt
@@ -1,11 +1,24 @@
 package com.example.mobilepharmacy
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class RegisterActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        val leftIcon: ImageView = findViewById(R.id.backButtonR)
+
+        leftIcon.setOnClickListener{
+            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
