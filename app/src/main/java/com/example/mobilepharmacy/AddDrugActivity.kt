85ff826ad2036cf2d428856edf0e33d8aab7ad8e
Patrycja Raczkowska <raczkowskapatrycja29@gmail.com>
2023-05-11 10:42:06 +0200

    Dodanie wyszukiwania leków

--- app/src/main/java/com/example/mobilepharmacy/AddDrugActivity.kt
+++ app/src/main/java/com/example/mobilepharmacy/AddDrugActivity.kt
@@ -7,6 +7,8 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 
 class AddDrugActivity : AppCompatActivity() {
 
@@ -56,6 +58,41 @@ class AddDrugActivity : AppCompatActivity() {
                 // Nie wykonujemy żadnej akcji
             }
         }
+
+        //        Lista leków
+        val drugsList: ArrayList<String> = ArrayList()
+        var xml_data = assets.open("Rejestr_Produktow_Leczniczych_calosciowy_stan_na_dzien_20230511.xml")
+        var factory: XmlPullParserFactory = XmlPullParserFactory.newInstance()
+        var parser: XmlPullParser = factory.newPullParser()
+
+        parser.setInput(xml_data, null)
+
+        var event: Int = parser.eventType
+        while (event != XmlPullParser.END_DOCUMENT) {
+            var tag_name: String = parser.name
+            when (event) {
+                XmlPullParser.END_TAG -> {
+                    if (tag_name == "produktLeczniczy") {
+                        var name: String = "\n" + parser.getAttributeValue(0)
+                        drugsList.add(name)
+                    }
+                }
+            }
+            event = parser.next()
+        }
+
+//       rozwijalne menu
+        val autoComplete: AutoCompleteTextView = findViewById(R.id.auto_complete_txt)
+
+        val adapter2 = ArrayAdapter(this, R.layout.list_drugs, drugsList)
+
+        autoComplete.setAdapter(adapter2)
+
+        autoComplete.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, view, i, l ->
+                val itemSelected = adapterView.getItemAtPosition(i)
+                Toast.makeText(this, "$itemSelected",Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun showCustomValueDialog() {
