4d6b5092b83d622527d621a98b4479aeca2dad4b
Patrycja Raczkowska <raczkowskapatrycja29@gmail.com>
2023-10-24 14:15:31 +0200

    Logowanie google

--- app/src/main/java/com/example/mobilepharmacy/AfterLoginActivity.kt
+++ app/src/main/java/com/example/mobilepharmacy/AfterLoginActivity.kt
@@ -3,26 +3,20 @@ package com.example.mobilepharmacy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.CalendarView
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.AppCompatButton
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
-
 class AfterLoginActivity : AppCompatActivity() {
 
     lateinit var calendarView: CalendarView
     lateinit var toggle: ActionBarDrawerToggle
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -148,7 +142,4 @@ class AfterLoginActivity : AppCompatActivity() {
             else -> return super.onOptionsItemSelected(item)
         }
     }
-
-
-
 }
--- app/src/main/java/com/example/mobilepharmacy/MainActivity.kt
+++ app/src/main/java/com/example/mobilepharmacy/MainActivity.kt
@@ -1,28 +1,33 @@
 package com.example.mobilepharmacy
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import android.widget.Button
+import android.widget.Toast
 import com.example.mobilepharmacy.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
-
-
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var launcher: ActivityResultLauncher<Intent> // Dodane
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val sharedPreferences = getSharedPreferences("login", MODE_PRIVATE)
         val email = sharedPreferences.getString("email", null)
@@ -33,17 +38,33 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AfterLoginActivity::class.java)
             startActivity(intent)
             finish()
-                } else {
-            setContentView(R.layout.activity_main)
         }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // logowanie z googlem
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Inicjalizacja launcher
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+        }
+
+        findViewById<Button>(R.id.loginButtonGoogle).setOnClickListener {
+            signInGoogle()
+        }
 
         // Ustawienie zdjęcia
         binding.imgStartViewImage.setImageResource(R.drawable.logo)
 
-        //Rejestracja
+        // Rejestracja
         binding.registerButton.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
@@ -54,4 +75,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent) // Uruchomienie intencji
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+        } else {
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val intent: Intent = Intent(this, AfterLoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
