d5fd6e34305b870d9016c5aa05a6d479131932ba
Patrycja Raczkowska <raczkowskapatrycja29@gmail.com>
2023-11-06 18:02:34 +0100

    naprawione logowanie z googlem, wszystko dodaje się do bazy

--- app/src/main/java/com/example/mobilepharmacy/MainActivity.kt
+++ app/src/main/java/com/example/mobilepharmacy/MainActivity.kt
@@ -1,114 +1,177 @@
 package com.example.mobilepharmacy
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mobilepharmacy.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
-    private lateinit var launcher: ActivityResultLauncher<Intent>
+    private lateinit var firestoreDB: FirebaseFirestore
+    private lateinit var firstName: String
+    private lateinit var lastName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sharedPreferences = getSharedPreferences("login", MODE_PRIVATE)
-        val email = sharedPreferences.getString("email", null)
-        val password = sharedPreferences.getString("password", null)
+        // Inicjalizacja Firebase Authentication
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        if (email != null && password != null) {
-            // Przekierowanie do AfterLoginActivity, jeśli użytkownik jest zalogowany
-            val intent = Intent(this, AfterLoginActivity::class.java)
-            startActivity(intent)
-            finish()
+        // Inicjalizacja Firestore
+        firestoreDB = FirebaseFirestore.getInstance()
+
+        // Ustawienie zdjęcia
+        binding.imgStartViewImage.setImageResource(R.drawable.logo)
+
+        binding.loginButton.setOnClickListener {
+            // Logowanie zwykłe
+            startActivity(Intent(this, Login::class.java))
         }
 
-        // logowanie z Google
+        binding.registerButton.setOnClickListener {
+            // Rejestracja
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        // Inicjalizacja opcji logowania Google
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
-        firebaseAuth = FirebaseAuth.getInstance()
+        // Inicjalizacja klienta logowania Google
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        // Inicjalizacja launcher
-        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                handleResults(task)
-            }
+        binding.loginButtonGoogle.setOnClickListener {
+            // Logowanie z Google
+            signInWithGoogle()
         }
 
-        findViewById<Button>(R.id.loginButtonGoogle).setOnClickListener {
-            signOutAndSignInGoogle()
-        }
-
-        // Ustawienie zdjęcia
-        binding.imgStartViewImage.setImageResource(R.drawable.logo)
-
-        // Rejestracja
-        binding.registerButton.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
-        }
-
-        // Logowanie
-        binding.loginButton.setOnClickListener {
-            startActivity(Intent(this, Login::class.java))
+        binding.loginButtonFacebook.setOnClickListener {
+            // Logowanie z Facebookiem
         }
     }
 
-    private fun signOutAndSignInGoogle() {
-        // Wyloguj użytkownika z Google
-        googleSignInClient.signOut().addOnCompleteListener {
-            if (it.isSuccessful) {
-                // Po wylogowaniu, ponownie uruchom logowanie z Google
-                val signInIntent = googleSignInClient.signInIntent
-                launcher.launch(signInIntent)
-            } else {
-                Toast.makeText(this, "Błąd wylogowywania z Google", Toast.LENGTH_SHORT).show()
+    // Dodaj funkcję do obsługi logowania z Google
+    private fun signInWithGoogle() {
+        val signInIntent: Intent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                // Pomyślnie zalogowano
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                // Logowanie nie powiodło się
+                Toast.makeText(this, "Logowanie przez Google nie powiodło się", Toast.LENGTH_SHORT).show()
             }
         }
     }
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Logowanie zakończone sukcesem
+                    val user = firebaseAuth.currentUser
+                    val email = user?.email.toString()
+                    Toast.makeText(this, "Zalogowano przez Google jako ${user?.displayName}", Toast.LENGTH_SHORT).show()
 
-    private fun handleResults(task: Task<GoogleSignInAccount>) {
-        if (task.isSuccessful) {
-            val account: GoogleSignInAccount? = task.result
-            if (account != null) {
-                updateUI(account)
+                    // Sprawdź, czy użytkownik jest zalogowany za pomocą Firebase Authentication
+                    if (user != null) {
+                        // Użytkownik jest zalogowany, więc możesz sprawdzić bazę danych
+                        checkIfUserExistsAndSignIn(user)
+                    } else {
+                        Toast.makeText(this, "Błąd logowania.", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // Logowanie nie powiodło się
+                    Toast.makeText(this, "Logowanie przez Google nie powiodło się", Toast.LENGTH_SHORT).show()
+                }
             }
-        } else {
-            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
-        }
     }
 
-    private fun updateUI(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+    // Sprawdzenie czy użytkownik googla już jest w bazie
+    private fun checkIfUserExistsAndSignIn(user: FirebaseUser) {
+        val usersCollection = firestoreDB.collection("users")
 
-        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
-            if (it.isSuccessful) {
-                val intent: Intent = Intent(this, AfterLoginActivity::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+        usersCollection.whereEqualTo("email", user.email)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    // Użytkownik o danym emailu istnieje w bazie
+                    val userDocument = querySnapshot.documents[0] // Zakładam, że jest tylko jeden użytkownik z danym emailem
+                    startActivity(Intent(this, AfterLoginActivity::class.java))
+                    // Zaloguj użytkownika lub wykonaj inne działania
+                } else {
+                    // Użytkownik o danym emailu nie istnieje w bazie, tworzymy nowy rekord
+                    createNewUser(user)
+                }
+            }
+            .addOnFailureListener { e ->
+                // Błąd podczas sprawdzania, co zrobić zależy od Twoich potrzeb
+                Toast.makeText(this, "Błąd podczas sprawdzania użytkownika: $e", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    // stworzenie nowego rekordu do bazy z użytkownikiem googla
+    private fun createNewUser(user: FirebaseUser) {
+        // podzielenie nazwy użytkownika na imię i nazwisko
+        val displayName = user.displayName
+        if (!displayName.isNullOrBlank()) {
+            val nameParts = displayName.split(" ") // Rozdziel na podstawie spacji (możesz użyć innego znaku)
+            if (nameParts.size >= 2) {
+                firstName = nameParts[0]
+                lastName = nameParts.subList(1, nameParts.size).joinToString(" ")
+            }else if (nameParts.size ==1)            {
+                firstName = nameParts[0]
             }
         }
+        // Tworzenie nowego rekordu użytkownika
+        val userData = hashMapOf(
+            "email" to user.email,
+            "userID" to user.uid,
+            "firstName" to firstName,
+            "lastName" to lastName
+        )
+
+        val userDocument = firestoreDB.collection("users").document() // Tworzy nowy dokument z unikalnym identyfikatorem
+        userDocument.set(userData)
+            .addOnSuccessListener {
+                // Rekord użytkownika został utworzony pomyślnie
+                // Przechodzimy dalej
+                startActivity(Intent(this, AfterLoginActivity::class.java))
+            }
+            .addOnFailureListener { e ->
+                // Błąd podczas tworzenia rekordu użytkownika
+                Toast.makeText(this, "Błąd podczas tworzenia rekordu użytkownika: $e", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
     }
 }
